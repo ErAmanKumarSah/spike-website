f14364a1b429ab6bf941c73573a14e37f133b20b
AmanKumar <amanjraj@gmail.com>
2017-04-14 18:21:26 +0530

    production js change

--- app.production.js
+++ app.production.js
@@ -2,15 +2,27 @@ const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const {UglifyJsPlugin} = require('webpack').optimize
 
+// module.exports = {
+//   // disable source maps
+//   devtool: false,
+//   // minify js
+//   plugins: [new UglifyJsPlugin()],
+//   // minify html and css
+//   reshape: htmlStandards({ minify: true }),
+//   postcss: cssStandards({
+//     minify: true,
+//     warnForDuplicates: false // cssnano includes autoprefixer
+//   })
+// }
 module.exports = {
-  // disable source maps
-  devtool: false,
-  // minify js
-  plugins: [new UglifyJsPlugin()],
-  // minify html and css
-  reshape: htmlStandards({ minify: true }),
-  postcss: cssStandards({
-    minify: true,
-    warnForDuplicates: false // cssnano includes autoprefixer
-  })
+  posthtml: (ctx) => {
+    return {
+      defaults: [
+        jade({
+          filename: ctx.resourcePath,
+          apiUrl: 'http://real-website.com/api/v1'
+        })
+      ]
+    }
+  }
 }
